74ab95cc48561f13c89886aaa63fc395c0cc259c
Cornelius Wichering <cornelius.wichering@engram.de>
2019-11-23 22:24:18 +0100

    Neuer View für "Meine Gruppen"

---
+++ lib/ManageGroups.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class ManageGroups extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Meine Gruppen'),
+      ),
+      body: Center(
+        child: Text('Yay'),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,78 +1,93 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-
-
+import 'package:flutter_train/ManageGroups.dart';
 
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext ctxt) {
+  Widget build(BuildContext context) {
     return new MaterialApp(
       home: new ListDisplay(),
     );
   }
 }
+
 class ListDisplay extends StatefulWidget {
   @override
   State createState() => new DynamicList();
 }
 
 class DynamicList extends State<ListDisplay> {
-  List<String> lItems = [];
+  List<String> trains = [];
   final TextEditingController eCtrl = new TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(title: new Text("Fluttr Train"),),
-      body: new Column(
-        children: <Widget>[
+        appBar: new AppBar(
+          title: new Text("xTrain"),
+        ),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              DrawerHeader(
+                child: Text('Mein xTrain'),
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                ),
+              ),
+              ListTile(
+                title: Text('Meine Gruppen'),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => ManageGroups()),
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
+        body: new Column(
+          children: <Widget>[
             new TextField(
               controller: eCtrl,
-              decoration: new InputDecoration(hintText: "Add new Train"),
-              onSubmitted: (text) {
-                if(text.length > 0) {
-                  lItems.add(text);
+              decoration: new InputDecoration(hintText: "Start a Train"),
+              onSubmitted: (trainName) {
+                if (trainName.length > 0) {
+                  trains.add(trainName);
                   eCtrl.clear();
                   setState(() {});
                 }
               },
             ),
-          new Expanded(
-              child: new ListView.builder
-                (
-                  itemCount: lItems.length,
-                  itemBuilder: (BuildContext ctxt, int Index) {
-                    return Card (
-                      child: Padding(
-                        padding: const EdgeInsets.all(16.0),
-                        child: Row (
-                          children: <Widget>[
-                            Expanded (
-                              child:
-                                Text('${lItems[Index]}',
-                                  style: TextStyle(fontSize: 22.0),),
-                            ),
-                            Expanded (
-                              
-                              child: Text("0"),
-                            )
-                          ],
-                        )
-
-                      ),
-                    );
-                  }
-              )
-          )
-        ],
-      )
-    );
+            new Expanded(
+                child: new ListView.builder(
+                    itemCount: trains.length,
+                    itemBuilder: (BuildContext buildContext, int index) {
+                      return Card(
+                        child: Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: Row(
+                              children: <Widget>[
+                                Expanded(
+                                  child: Text(
+                                    '${trains[index]}',
+                                    style: TextStyle(fontSize: 22.0),
+                                  ),
+                                ),
+                                Expanded(
+                                  child: Text("0"),
+                                )
+                              ],
+                            )),
+                      );
+                    }))
+          ],
+        ));
   }
 }
-
-
-
 
 /*void main() {
   //List<String> tmpList = ["Meking", "Aldi", "Tankstelle"];
