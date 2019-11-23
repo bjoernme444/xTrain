b9c98c61b68c36775da3b1d584528eaa887f9532
Cornelius Wichering <cornelius.wichering@engram.de>
2019-11-24 00:01:53 +0100

    Kann jetzt Gruppen anlegen

--- lib/ManageGroups.dart
+++ lib/ManageGroups.dart
@@ -1,9 +1,44 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 import 'GroupList.dart';
 
 class ManageGroups extends StatelessWidget {
+  TextEditingController _textFieldController = TextEditingController();
+
+  _displayDialog(BuildContext context) async {
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text('Neue Gruppe'),
+            content: TextField(
+              controller: _textFieldController,
+              decoration: InputDecoration(hintText: "Name der Gruppe..."),
+            ),
+            actions: <Widget>[
+              new FlatButton(
+                child: new Text('Abbrechen'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+              new FlatButton(
+                child: new Text('Anlegen'),
+                onPressed: () {
+                  print('Pressed: ${_textFieldController.value.text}');
+                  final value = _textFieldController.value.text;
+                  Firestore.instance.collection('groups').add({'name': value}).then((_) {
+                    Navigator.of(context).pop();
+                  });
+                },
+              )
+            ],
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,7 +46,12 @@ class ManageGroups extends StatelessWidget {
         title: Text('Meine Gruppen'),
       ),
       body: Center(
-        child: GroupList()
+        child: GroupList(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => _displayDialog(context),
+        backgroundColor: Colors.green,
       ),
     );
   }
